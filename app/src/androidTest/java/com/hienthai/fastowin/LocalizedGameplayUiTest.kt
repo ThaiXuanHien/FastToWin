e12ebd32975a2ee2b21b673d2fecc1576259b1a9
Hien Hue <xtit6789@gmail.com>
2026-09-07 21:12:02 +0700

    test: fix Compose assertion import

--- app/src/androidTest/java/com/hienthai/fastowin/LocalizedGameplayUiTest.kt
+++ app/src/androidTest/java/com/hienthai/fastowin/LocalizedGameplayUiTest.kt
@@ -1,7 +1,6 @@
 package com.hienthai.fastowin
 
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.v2.createComposeRule
