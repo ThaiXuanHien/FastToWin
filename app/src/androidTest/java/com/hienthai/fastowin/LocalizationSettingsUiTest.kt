acacf8eeada18886d24945bacdb47d92e955b378
Hien Hue <xtit6789@gmail.com>
2026-09-17 15:44:55 +0700

    test: fix localization UI test import

--- app/src/androidTest/java/com/hienthai/fastowin/LocalizationSettingsUiTest.kt
+++ app/src/androidTest/java/com/hienthai/fastowin/LocalizationSettingsUiTest.kt
@@ -2,7 +2,6 @@ package com.hienthai.fastowin
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
